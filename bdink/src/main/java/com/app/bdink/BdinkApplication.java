da1087021417cb5931d1de63824996e783f39521
soohyun <sss4920@naver.com>
2025-02-02 16:18:23 +0900

    feature(BdinkApplication.java): 초기세팅

---
+++ bdink/src/main/java/com/app/bdink/BdinkApplication.java
@@ -0,0 +1,13 @@
+package com.app.bdink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdinkApplication.class, args);
+	}
+
+}
---
+++ bdink/src/test/java/com/app/bdink/BdinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bdink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
