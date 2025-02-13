51fc478e95101fa8acff333bb38ac1d61ddb3a46
soohyun <sss4920@naver.com>
2025-02-13 19:45:30 +0900

    testing/#2(PriceDetailTest): 가격 할인 테스트코드 추가

---
+++ bdink/src/test/java/com/app/bdink/classroom/domain/PriceDetailTest.java
@@ -0,0 +1,28 @@
+package com.app.bdink.classroom.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PriceDetailTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "10000,         0.3,        7000",
+            "10000,         0.2,        8000",
+            "0,             0.2,        0",
+    })
+    @DisplayName("할인율을 주면 할인된 가격을 리턴한다.")
+    public void returnDiscountedPrice(int discountPrice, float discountRate, int result){
+        //given
+        PriceDetail priceDetail = new PriceDetail(discountPrice, discountRate);
+
+        //when
+        int expected = priceDetail.getDiscountPrice();
+
+        Assertions.assertEquals(expected, result);
+    }
+}
