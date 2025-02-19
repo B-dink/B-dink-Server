9d125e8f256477d84edf45aada7f13f147f75d0d
soohyun <sss4920@naver.com>
2025-02-19 19:32:01 +0900

    feature(swagger): swagger 관련 설명 추가

--- bdink/src/main/java/com/app/bdink/lecture/controller/InstructorController.java
+++ bdink/src/main/java/com/app/bdink/lecture/controller/InstructorController.java
@@ -4,6 +4,8 @@ import com.app.bdink.lecture.controller.dto.InstructorDto;
 import com.app.bdink.lecture.service.InstructorService;
 import com.app.bdink.member.entity.Member;
 import com.app.bdink.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +15,14 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/instructor")
+@Tag(name = "강사 API", description = "강사와 관련된 API들입니다. 강사는 회원가입해 Member 엔티티가 생성된 이후에 생성할 수 있습니다.")
 public class InstructorController {
 
     private final InstructorService instructorService;
     private final MemberService memberService;
 
     @PostMapping
+    @Operation(method = "POST", description = "강사 정보를 생성합니다.")
     public ResponseEntity<?> createInstructor(@RequestParam Long memberId, @RequestBody InstructorDto instructorDto){
 
         Member member = memberService.findById(memberId);
--- bdink/src/main/java/com/app/bdink/lecture/controller/LectureController.java
+++ bdink/src/main/java/com/app/bdink/lecture/controller/LectureController.java
@@ -9,6 +9,9 @@ import com.app.bdink.lecture.service.ChapterService;
 import com.app.bdink.lecture.service.LectureService;
 import com.app.bdink.member.entity.Member;
 import com.app.bdink.member.service.MemberService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,7 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/lecture")
+@Tag(name = "강의 API", description = "강의와 관련된 API들입니다. 강의는 클래스룸을 생성하고, 챕터가 생성되어있어야하며, 해당 챕터에 생성해야합니다.")
 public class LectureController {
 
     private final LectureService lectureService;
@@ -26,6 +30,7 @@ public class LectureController {
 
     //TODO: 강사인지 확인하는 로직이 필요하다.
     @PostMapping
+    @Operation(method = "POST", description = "강의를 생성합니다. 강의를 생성하면 해당 강의에 대한 시간정보, 강의 수가 해당 챕터에 업데이트 되는 로직입니다.")
     public ResponseEntity<?> createLecture(@RequestParam Long classRoomId,
                                             @RequestParam Long chapterId,
                                             @RequestBody LectureDto lectureDto) {
@@ -38,6 +43,7 @@ public class LectureController {
     }
 
     @GetMapping
+    @Operation(method = "GET", description = "강의 id를 받아 해당 강의의 정보를 조회합니다.")
     public ResponseEntity<?> getLectureInfo(@RequestParam Long id) {
 
         LectureInfo lectureInfo = lectureService.getLectureInfo(id);
@@ -46,6 +52,7 @@ public class LectureController {
     }
 
     @DeleteMapping
+    @Operation(method = "DELETE", description = "강의를 삭제합니다. 현재 하드 delete로 구성되어있습니다.")
     ResponseEntity<?> deleteLecture(@RequestParam Long id){
         lectureService.deleteLecture(id);
         return ResponseEntity.noContent().build();
