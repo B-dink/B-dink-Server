28823dbbe96c799b2fcb7ee718ac5ea9809af89c
ckr8305 <ckr8305@naver.com>
2025-02-20 15:19:27 +0900

    fix/#7(NewsService): 스웨거 403 에러 해결 및 기능 추가

--- bdink/src/main/java/com/app/bdink/global/config/SecurityConfig.java
+++ bdink/src/main/java/com/app/bdink/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,7 +15,9 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+            );
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
