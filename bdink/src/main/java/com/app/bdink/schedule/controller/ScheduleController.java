54616297c14eb5a2d8d9dfd45b5c2e990e103bb7
ckr8305 <ckr8305@naver.com>
2025-03-01 19:56:54 +0900

    refactor/#24(ScheduleController): 스웨거 설명 변경

--- bdink/src/main/java/com/app/bdink/schedule/controller/ScheduleController.java
+++ bdink/src/main/java/com/app/bdink/schedule/controller/ScheduleController.java
@@ -19,12 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/schedule")
-@Tag(name = "Schedule API", description = "일정과 관련된 API들입니다.")
+@Tag(name = "캘린더 API", description = "대회, 재활, 영양 캘린더와 관련된 API들입니다.")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    @Operation(method = "POST", description = "일정을 생성합니다.")
+    @Operation(method = "POST", description = "캘린더 일정을 생성합니다.")
     @PostMapping
     public ResponseEntity<?> createSchedule(@RequestBody ScheduleRequest scheduleRequest) {
         String id = scheduleService.createSchedule(scheduleRequest);
@@ -33,26 +33,26 @@ public class ScheduleController {
             .build();
     }
 
-    @Operation(method = "GET", description = "모든 일정을 조회합니다.")
+    @Operation(method = "GET", description = "모든 캘린더 일정을 조회합니다.")
     @GetMapping
     public ResponseEntity<?> getAllSchedule() {
         return ResponseEntity.ok().body(scheduleService.getAllSchedule());
     }
 
-    @Operation(method = "GET", description = "카테고리 별 일정을 조회합니다.")
+    @Operation(method = "GET", description = "카테고리 별 캘린더 일정을 조회합니다.")
     @GetMapping("/type")
     public ResponseEntity<?> getScheduleByCategory(@RequestParam String scheduleType) {
         return ResponseEntity.ok().body(scheduleService.getScheduleByScheduleType(scheduleType.toUpperCase()));
     }
 
-    @Operation(method = "PUT", description = "일정을 수정합니다.")
+    @Operation(method = "PUT", description = "캘린더 일정을 수정합니다.")
     @PutMapping
     public ResponseEntity<?> updateSchedule(@RequestParam Long id, @RequestBody ScheduleRequest scheduleRequest) {
         scheduleService.updateSchedule(id, scheduleRequest);
         return ResponseEntity.ok().build();
     }
 
-    @Operation(method = "DELETE", description = "일정을 삭제합니다.")
+    @Operation(method = "DELETE", description = "캘린더 일정을 삭제합니다.")
     @DeleteMapping
     public ResponseEntity<?> deleteSchedule(@RequestParam Long id) {
         scheduleService.deleteSchedule(id);
