882283a62dfcd6c8b42bd52c05fa0f690b164b60
soohyun <sss4920@naver.com>
2025-03-18 18:58:42 +0900

    hotfix(InstructorUtilService)/#82: 권한 판단 에러 수정

--- bdink/src/main/java/com/app/bdink/lecture/controller/ChapterController.java
+++ bdink/src/main/java/com/app/bdink/lecture/controller/ChapterController.java
@@ -12,6 +12,7 @@ import com.app.bdink.lecture.service.ChapterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,7 @@ import java.security.Principal;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/chapter")
 @Tag(name = "챕터 API", description = "챕터와 관련된 API들입니다. 챕터는 챕터마다 강좌에 대한 데이터를 총합해 가지고 있습니다. 클래스룸을 만들어야 생성할 수 있습니다.")
+@Slf4j
 public class ChapterController {
 
     private final ChapterService chapterService;
@@ -35,10 +37,9 @@ public class ChapterController {
                                            @RequestParam Long classRoomId,
                                            @RequestParam String title) {
 
-        if (instructorUtilService.validateClassRoomOwner(principal, classRoomId)){
+        if (!instructorUtilService.validateClassRoomOwner(principal, classRoomId)){
             throw new CustomException(Error.UNAUTHORIZED_ACCESS, Error.UNAUTHORIZED_ACCESS.getMessage());
         }
-
         ClassRoomEntity classRoomEntity = classRoomService.findById(classRoomId);
 
         String chapterId = chapterService.createChapter(classRoomEntity, title);
