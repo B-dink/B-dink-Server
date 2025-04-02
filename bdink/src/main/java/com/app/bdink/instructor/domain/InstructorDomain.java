37685e8349707196694359aad158e83efdb23279
Lee, yeeun <ye7426@naver.com>
2025-04-02 17:56:58 +0900

    feature/#107(instructor): instructor package 분리

---
+++ bdink/src/main/java/com/app/bdink/instructor/domain/InstructorDomain.java
@@ -0,0 +1,28 @@
+package com.app.bdink.instructor.domain;
+
+import com.app.bdink.classroom.domain.Career;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class InstructorDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Career career;
+
+    public void modify(final Career career){
+        this.career = career;
+    }
+
+    public void softDelete(){
+        this.career = null;
+    }
+
+}
