c4956c48da99c811c47ce9734cfc440ca6d5ec96
Caruh <miso4206@gmail.com>
2025-04-21 22:53:14 +0900

    feature/#144(Kollus): Kollus 관련Repository 생성

--- bdink/src/main/java/com/app/bdink/external/kollus/repository/KollusMediaLinkRepository.java
+++ bdink/src/main/java/com/app/bdink/external/kollus/repository/KollusMediaLinkRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface KollusMediaLinkRepository extends JpaRepository<KollusMediaLink, Long> {
     // 특정 사용자가 어떤 미디어 콘텐츠의 accessToken을 요청
-    Optional<KollusMediaLink> findByMemberIdAndMediaContentKey(Long memberId, String mediaContentKey);
+    Optional<KollusMediaLink> findByMemberIdAndKollusMediaId(Long memberId, Long kollusMediaId);
 
     // 사용자별 등록된 영상 리스트 가져오기
     List<KollusMediaLink> findAllByMemberId(Long memberId);
---
+++ bdink/src/main/java/com/app/bdink/external/kollus/repository/KollusMediaRepository.java
@@ -0,0 +1,12 @@
+package com.app.bdink.external.kollus.repository;
+
+import com.app.bdink.external.kollus.entity.KollusMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface KollusMediaRepository extends JpaRepository<KollusMedia, Long> {
+    Optional<KollusMedia> findByLectureId (Long lectureId);
+}
+
