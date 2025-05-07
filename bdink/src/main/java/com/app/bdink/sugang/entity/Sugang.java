760278ae9644a0638baf1a5f203b9cde25b903a8
Caruh <miso4206@gmail.com>
2025-05-07 15:44:09 +0900

    feature/#158(Sugang) 수강 엔티티 필드 변경

--- bdink/src/main/java/com/app/bdink/sugang/entity/Sugang.java
+++ bdink/src/main/java/com/app/bdink/sugang/entity/Sugang.java
@@ -1,8 +1,8 @@
 package com.app.bdink.sugang.entity;
 
-import com.app.bdink.external.aws.lambda.domain.Media;
-import com.app.bdink.lecture.entity.Lecture;
+import com.app.bdink.classroom.adapter.out.persistence.entity.ClassRoomEntity;
 import com.app.bdink.member.entity.Member;
+import com.app.bdink.sugang.controller.dto.SugangStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -19,46 +19,37 @@ public class Sugang {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "lecture_id")
-    private Lecture lecture;
+    private ClassRoomEntity classRoomEntity;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Media media;
-
-    @Getter
-    @Column(name = "completed")
-    private boolean completed;
-
-    private double progress;
+    @Enumerated(EnumType.STRING)
+    private SugangStatus sugangStatus;
 
     @Builder
-    public Sugang(Lecture lecture, Member member, Media media) {
-        this.lecture = lecture;
+    public Sugang(ClassRoomEntity classRoomEntity, Member member, SugangStatus sugangStatus) {
+        this.classRoomEntity = classRoomEntity;
         this.member = member;
-        this.media = media;
-        this.completed = false;  // 기본값: 수강하지 않은 상태
-        this.progress = 0;
+        this.sugangStatus = sugangStatus;
     }
 
-    public double updateProgress(){
-        if (this.progress > 100){
-            this.progress = 100;
-            completed = true;
-            return 100;
-        }
-        double totalSize = media.getTotalLength();
-        double presentSize = (double) totalSize / 10;
-        this.progress += presentSize *100;
-
-        if (this.progress > 100){
-            this.progress=100;
-            completed = true;
-        }
-
-        return this.progress;
-    }
+//    public double updateProgress(){
+//        if (this.progress > 100){
+//            this.progress = 100;
+//            completed = true;
+//            return 100;
+//        }
+//        double totalSize = media.getTotalLength();
+//        double presentSize = (double) totalSize / 10;
+//        this.progress += presentSize *100;
+//
+//        if (this.progress > 100){
+//            this.progress=100;
+//            completed = true;
+//        }
+//
+//        return this.progress;
+//    }
 
 }
