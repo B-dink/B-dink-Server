a26cf84d9117a7a1f6edd26304db56e0009cf918
Caruh <miso4206@gmail.com>
2025-05-23 11:30:32 +0900

    add/#247(lectureResponse) 강좌 아이디 응답 DTO 추가

--- bdink/src/main/java/com/app/bdink/lecture/controller/dto/response/LectureResponse.java
+++ bdink/src/main/java/com/app/bdink/lecture/controller/dto/response/LectureResponse.java
@@ -3,12 +3,13 @@ package com.app.bdink.lecture.controller.dto.response;
 import com.app.bdink.lecture.entity.Lecture;
 
 public record LectureResponse(
-    String title,
-    String lectureTime,
-    String progress
+        Long lectureId,
+        String title,
+        String lectureTime,
+        String progress
 ) {
     public static LectureResponse from(final Lecture lecture, final double watchProgress) {
         String progressStr = watchProgress >= 90 ? "완강" : Math.round(watchProgress) + "%";
-        return new LectureResponse(lecture.getTitle(), lecture.getTime().toString(), progressStr);
+        return new LectureResponse(lecture.getId(), lecture.getTitle(), lecture.getTime().toString(), progressStr);
     }
 }
