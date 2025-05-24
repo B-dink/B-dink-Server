23b162d1136f239b312ce589b535952d40348347
Joon9750 <junhyeok2166@daum.net>
2025-05-24 16:59:07 +0900

    [chore] BookmarkResponse 수정

--- bdink/src/main/java/com/app/bdink/bookmark/adapter/in/controller/dto/response/BookmarkResponse.java
+++ bdink/src/main/java/com/app/bdink/bookmark/adapter/in/controller/dto/response/BookmarkResponse.java
@@ -5,15 +5,19 @@ import com.app.bdink.classroom.adapter.out.persistence.entity.ClassRoomEntity;
 public record BookmarkResponse(
     String classRoomTitle,
     String instructor,
-    Long classRoomId
+    Long classRoomId,
+    String thumbnail,
+    Long totalLectureCount
 ) {
 
-    public static BookmarkResponse from(final ClassRoomEntity classRoomEntity) {
+    public static BookmarkResponse from(final ClassRoomEntity classRoomEntity, final Long totalLectureCount) {
         return new BookmarkResponse(
             classRoomEntity.getTitle(),
             // TODO: instructor 고민해보기
             classRoomEntity.getInstructor().getMember().getName(),
-            classRoomEntity.getId()
+            classRoomEntity.getId(),
+            classRoomEntity.getThumbnail(),
+            totalLectureCount
         );
     }
 }
