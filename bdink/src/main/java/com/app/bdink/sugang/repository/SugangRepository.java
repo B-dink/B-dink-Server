0ef7b6bd22b1c4b2d76d19b7172c392e6e685f23
Fidesss <miso4206@gmail.com>
2025-05-25 02:43:47 +0900

    feature/#266(Sugang) 만료시간 자동 스케줄러 생성

--- bdink/src/main/java/com/app/bdink/sugang/repository/SugangRepository.java
+++ bdink/src/main/java/com/app/bdink/sugang/repository/SugangRepository.java
@@ -6,11 +6,14 @@ import com.app.bdink.sugang.controller.dto.SugangStatus;
 import com.app.bdink.sugang.entity.Sugang;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface SugangRepository extends JpaRepository<Sugang, Long> {
     List<Sugang> findAllByMember(Member member);
+
+    List<Sugang> findByExpiredDateBeforeAndSugangStatus(LocalDate now, SugangStatus status);
 
     Optional<Sugang> findByMemberAndClassRoomEntity(Member member, ClassRoomEntity classRoomEntity);
     
