778aaab9004a80a974d3482b3f0a7c5c8db78f1f
Joon9750 <junhyeok2166@daum.net>
2025-05-30 14:42:45 +0900

    [hotfix] null exception 해결

--- bdink/src/main/java/com/app/bdink/classroom/adapter/in/controller/dto/response/AllCareerClassRoomResponse.java
+++ bdink/src/main/java/com/app/bdink/classroom/adapter/in/controller/dto/response/AllCareerClassRoomResponse.java
@@ -3,7 +3,10 @@ package com.app.bdink.classroom.adapter.in.controller.dto.response;
 import com.app.bdink.chapter.domain.ChapterSummary;
 import com.app.bdink.classroom.adapter.out.persistence.entity.ClassRoomEntity;
 import com.app.bdink.classroom.domain.Career;
+import com.app.bdink.instructor.adapter.out.persistence.entity.Instructor;
+import com.app.bdink.member.entity.Member;
 import com.app.bdink.price.domain.PriceDetail;
+import java.util.Optional;
 
 public record AllCareerClassRoomResponse(
         Long id,
@@ -18,12 +21,17 @@ public record AllCareerClassRoomResponse(
         int totalReviewCount
 ) {
     public static AllCareerClassRoomResponse of(final ClassRoomEntity classRoomEntity, final ChapterSummary chapterSummary, Boolean isBookmarked, int totalReviewCount){
+        String instructorName = Optional.ofNullable(classRoomEntity.getInstructor())
+                .map(Instructor::getMember)
+                .map(Member::getName)
+                .orElse("강사 정보 없음");
+
         return new AllCareerClassRoomResponse(
                 classRoomEntity.getId(),
                 classRoomEntity.getCareer(),
                 classRoomEntity.getTitle(),
                 classRoomEntity.getThumbnail(),
-                classRoomEntity.getInstructor().getMember().getName(),
+                instructorName,
                 classRoomEntity.getPriceDetail(),
                 isBookmarked,
                 chapterSummary.getTotalLectureCount(),
