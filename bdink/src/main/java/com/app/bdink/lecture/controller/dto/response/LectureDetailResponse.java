3a9f4ddec32612d94cb6c18466ec6cd9b82c4b1c
Fidesss <miso4206@gmail.com>
2025-06-20 19:39:16 +0900

    hotfix(LectureDetailResponse) LectureDetailResponse 200 성공 응답 classRoomId 데이터 추가

--- bdink/src/main/java/com/app/bdink/lecture/controller/dto/response/LectureDetailResponse.java
+++ bdink/src/main/java/com/app/bdink/lecture/controller/dto/response/LectureDetailResponse.java
@@ -3,12 +3,13 @@ package com.app.bdink.lecture.controller.dto.response;
 import com.app.bdink.lecture.entity.Lecture;
 
 public record LectureDetailResponse(
+        Long classRoomId,
         Long lectureId,
         String lectureTitle,
         String lectureTime
 ) {
     public static LectureDetailResponse from(final Lecture lecture) {
-        return new LectureDetailResponse(lecture.getId(), lecture.getTitle(), lecture.getTime().toString());
+        return new LectureDetailResponse(lecture.getClassRoom().getId(), lecture.getId(), lecture.getTitle(), lecture.getTime().toString());
     }
 }
 
