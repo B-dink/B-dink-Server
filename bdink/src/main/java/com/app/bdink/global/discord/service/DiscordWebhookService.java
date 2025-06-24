c8083ec1267a010820683cfc5e7ddb3913622899
Caruh <miso4206@gmail.com>
2025-06-24 12:51:36 +0900

    feature/#331(DiscordWebhook) Discord url yaml 변수로 대체

--- bdink/src/main/java/com/app/bdink/global/discord/service/DiscordWebhookService.java
+++ bdink/src/main/java/com/app/bdink/global/discord/service/DiscordWebhookService.java
@@ -1,6 +1,7 @@
 package com.app.bdink.global.discord.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,13 +10,13 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class DiscordWebhookService {
+    @Value("${discord.WEBHOOK-URL}")
+    private String WEBHOOK_URL;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    private final String webhookUrl = "https://discord.com/api/webhooks/1386490380354060298/q7vtDsZ6NhuHxor13Vukk_tHitQPf8Db8M2fcGg1ln8-u802QMHXXYKGD9atn4F4WMUp"; //Todo: 현재 테스트 url, 실제 URL로 교체 필요
-
     public void sendMessage(String message) {
         Map<String, String> payload = Map.of("content", message);
-        restTemplate.postForEntity(webhookUrl, payload, String.class);
+        restTemplate.postForEntity(WEBHOOK_URL, payload, String.class);
     }
 }
