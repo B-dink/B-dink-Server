8058803c0b4d778b8bbe0c64445c4676325decbb
Joon9750 <junhyeok2166@daum.net>
2025-08-08 22:38:46 +0900

    [Chore] 주석 제거

--- bdink/src/main/java/com/app/bdink/sugang/service/SugangService.java
+++ bdink/src/main/java/com/app/bdink/sugang/service/SugangService.java
@@ -18,7 +18,6 @@ import com.app.bdink.sugang.entity.Sugang;
 import com.app.bdink.sugang.repository.SugangRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronization;
