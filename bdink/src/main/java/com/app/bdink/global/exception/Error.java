1f6630b26fff1ffc05da794e1ccf7f65cf4783bc
Caruh <miso4206@gmail.com>
2025-09-13 11:51:52 +0900

    feature/#376(Error) QR코드 검증 실패 응답 추가

--- bdink/src/main/java/com/app/bdink/global/exception/Error.java
+++ bdink/src/main/java/com/app/bdink/global/exception/Error.java
@@ -128,6 +128,7 @@ public enum Error {
     UN_AUTHORIZED(HttpStatus.UNAUTHORIZED, "토큰 인증을 안한 유저입니다."),
     UNAUTHORIZED_KEY(HttpStatus.UNAUTHORIZED, "인증되지 않은 시크릿 키 혹은 클라이언트 키 입니다."),
     INVALID_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    INVALID_QR_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED, "유효하지 않은 QR토큰입니다."),
 
     /**
      * 402 PAYMENT REQUIRED
