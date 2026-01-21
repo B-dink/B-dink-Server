4c925aa15fa676b388f3258b05e80d631e788ea7
Caruh <miso4206@gmail.com>
2026-01-21 21:33:54 +0900

    modify/#436(Notification) 트레이너 qr등록시 Notification 레코드 생성 로직 추가

--- bdink/src/main/java/com/app/bdink/notification/entity/NotificationLinkType.java
+++ bdink/src/main/java/com/app/bdink/notification/entity/NotificationLinkType.java
@@ -3,6 +3,8 @@ package com.app.bdink.notification.entity;
 public enum NotificationLinkType {
     // 내 트레이너 관리 화면
     TRAINER_PROFILE,
+    // 트레이너 회원 목록
+    TRAINER_MEMBER_LIST,
     //TODO: 콘텐츠 상세 (강의/리뷰 등) 이 부분은 어떤 페이지로 이동을 해야하려나
     CONTENT_DETAIL,
     // 운동일지 상세
