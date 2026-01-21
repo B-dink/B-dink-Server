4dc705556b2d7e35e4b4311521e8047c1bc872d8
Caruh <miso4206@gmail.com>
2026-01-22 00:53:08 +0900

    feature/#438(DeviceToken) FCM DeviceToken 엔티티 생성

---
+++ bdink/src/main/java/com/app/bdink/notification/repository/DeviceTokenRepository.java
@@ -0,0 +1,12 @@
+package com.app.bdink.notification.repository;
+
+import com.app.bdink.notification.entity.DeviceToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
+    Optional<DeviceToken> findByToken(String token);
+    List<DeviceToken> findAllByMemberIdAndIsActiveTrueAndIsAllowedTrue(Long memberId);
+}
