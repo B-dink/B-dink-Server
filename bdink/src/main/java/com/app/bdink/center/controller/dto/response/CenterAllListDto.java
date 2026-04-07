35e3f13ddb3ea99e614767566c89027f6c2f44c9
Caruh <miso4206@gmail.com>
2026-04-07 16:29:23 +0900

    develop(CenterAllListDto) 모든 센터 조회 api centerId 필드 추가

--- bdink/src/main/java/com/app/bdink/center/controller/dto/response/CenterAllListDto.java
+++ bdink/src/main/java/com/app/bdink/center/controller/dto/response/CenterAllListDto.java
@@ -3,11 +3,13 @@ package com.app.bdink.center.controller.dto.response;
 import com.app.bdink.center.entity.Center;
 
 public record CenterAllListDto(
+    Long centerId,
     String centerName,
     String centerAddress
 ) {
     public static CenterAllListDto of(Center center){
         return new CenterAllListDto(
+                center.getId(),
                 center.getName(),
                 center.getAddress()
         );
